279da20caec69a8fc02abc8cb62dfb7fdbe956bf
Irmawati567 <irmawatiiklll@gmail.com>
2024-05-08 22:29:01 +0700

    project01

---
+++ tugas/tugas/footer.php
@@ -0,0 +1,9 @@
+<footer class="text-center py-3 bg-light mt-5">
+
+                <p>Created by <a href="#">Sevina Citra</a> &copy; <?php echo date('Y') ?></p>
+       
+            </footer>
+
+</body>
+
+</html>
